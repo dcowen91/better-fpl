75f326f4c3652a5b86216f2e900032287457f7a5
Jeppe Smith <mail@jeppesmith.dk>
2016-08-02 23:27:55 +0200

    Remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,5 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: true, sourcemap: false }),
-  ],
-  loaders: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
-      query: {
-        presets: ['es2015']
-      }
-    }
   ]
 }
